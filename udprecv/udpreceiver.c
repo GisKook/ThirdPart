aaf033b1eaf920781221c06abeae303d67d19b0c
GisKook <zhangkai.gis@163.com>
2014-08-17 20:07:35 -0700

    tobe rebuild

--- udprecv/udpreceiver.c
+++ udprecv/udpreceiver.c
@@ -11,11 +11,10 @@
 
 
 int main(){
-	printf("dd");
 	int udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
 	if(udp_socket == -1){
 		//	handle_error("socket");
-		fprintf(stderr, "socket error");
+		fprintf(stderr, "socket error\n");
 	}
 	struct sockaddr_in client_addr;
 
@@ -25,7 +24,7 @@ int main(){
 	//client_addr.sin_addr.s_addr = inet_addr("192.168.1.155");
 	client_addr.sin_port = htons(20001);
 	if(bind(udp_socket,(struct sockaddr*)&client_addr, sizeof(struct sockaddr_in)) == -1){
-		fprintf(stderr, "bind error");
+		fprintf(stderr, "bind error\n");
 	} 
 
 	struct sockaddr_in server_addr;
@@ -35,109 +34,67 @@ int main(){
 	server_addr.sin_addr.s_addr = inet_addr("192.168.1.115");
 	server_addr.sin_port = htons(20000);
 	if(connect(udp_socket,(struct sockaddr*)&server_addr, sizeof(struct sockaddr_in)) == -1){
-		fprintf(stderr, "connect error");
+		fprintf(stderr, "connect error\n");
 	} 
 
-	int flag = fcntl(udp_socket, F_GETFD, 0);
-	printf("%d", flag);
+	int flag = 0;
+	flag = fcntl(udp_socket, F_GETFD, 0);
 
-//	if(-1==fcntl(udp_socket,F_SETFD, flag | O_NONBLOCK)){
-//		fprintf(stderr, "fcntl error");
-//	}
-//	flag = fcntl(STDIN_FILENO, F_GETFD, 0);
-//	if(-1==fcntl(STDIN_FILENO, F_SETFD,flag & !O_NONBLOCK)){
-//		fprintf(stderr, "stdin nonblock error");
-//	}
+	if(-1==fcntl(udp_socket,F_SETFD, flag | O_NONBLOCK)){
+		fprintf(stderr, "fcntl error\n");
+	}
+	flag = fcntl(STDIN_FILENO, F_GETFD, 0);
+	if(-1==fcntl(STDIN_FILENO, F_SETFD,flag | O_NONBLOCK)){
+		fprintf(stderr, "stdin nonblock error\n");
+	}
 
 	int efd = epoll_create1(EPOLL_CLOEXEC);
 	if( efd == -1){
-		fprintf(stderr, "epoll_crate error");
+		fprintf(stderr, "epoll_crate error\n");
 	}
 	struct epoll_event ev;
 	ev.events = EPOLLIN | EPOLLET;
 	ev.data.fd = udp_socket;
 	if (epoll_ctl(efd, EPOLL_CTL_ADD, udp_socket, &ev) == -1) {
-		fprintf(stderr, "add fd error");
+		fprintf(stderr, "add fd error\n");
 	}
 
 	struct epoll_event evstdin;
 	evstdin.events = EPOLLIN | EPOLLET;
 	evstdin.data.fd = STDIN_FILENO;
 	if (epoll_ctl(efd, EPOLL_CTL_ADD, STDIN_FILENO, &evstdin) == -1) {
-		fprintf(stderr, "add fd error");
+		fprintf(stderr, "add fd error\n");
 	}
 
 	int n = 0,i=0;
 	struct epoll_event ev1[4];
-	char buf[1024]={0};
+	char buf[10240]={0};
 
-	//	struct msghdr msg;
-	//	msg.msg_name = NULL;
-	//	msg.msg_namelen = 0;
-	//	struct iovec io;
-	//	io.iov_base = buf;
-	//	io.iov_len = 1024;
-	//	msg.msg_iov = &io;
-	//	msg.msg_iovlen = 1;
-	//	ssize_t len;
-		ssize_t len;
+	ssize_t len;
 	for(;;){
 		n = epoll_wait(efd, ev1, 4, -1);
 
 		for(i = 0; i < n; i++){
 			uint32_t flags = ev1[i].events;
-			if(flags&EPOLLIN){
+			if( (flags & EPOLLERR) || 
+					(flags & EPOLLHUP) ||
+					!(flags & EPOLLIN)){
+				fprintf(stderr, "epoll error!\n");
+				close(ev1[i].data.fd); 
+				continue;
+			}else{
 				printf("recv ");
-				memset(buf, 0, 1024);
+				memset(buf, 0, 10240);
 				errno = 0;
-				
-				len = read(ev1[i].data.fd,buf,1024);
-				printf(" %s ", buf);
-				if(errno == EAGAIN){
-					printf("EAGAIN");
-				}
-//				while(read(ev1[i].data.fd,buf,1024) > 0){ 
-//					printf("%s -- %d\n",buf, len);
-//					if(errno == EAGAIN){
-//						break;
-//					}
-//				}
-//				if(ev1[i].data.fd == STDIN_FILENO){ 
-//					tcflush(STDIN_FILENO,TCIFLUSH);
-//				}
-	//			sleep(2);
-				//			len = recvmsg(ev1[i].data.fd, &msg, 0);
-				//			if(msg.msg_flags & MSG_TRUNC){
-				//				printf("trunc\n");
 
-				//			}
+				len = read(ev1[i].data.fd,buf,10240);
+				printf(" %s \n", buf);
+				if(errno != 0){
+					printf("errno is %d \n", errno);
+				}
 			}
 
+
 		}
-
-
-		//	const int MAXLEN = 1024;
-		//	char buf[MAXLEN+1];
-		//	char buf2[MAXLEN+1];
-		//
-		//	struct msghdr msg;
-		//	msg.msg_name = NULL;
-		//	msg.msg_namelen = 0;
-		//	struct iovec io;
-		//	io.iov_base = buf;
-		//	io.iov_len = MAXLEN;
-		//	msg.msg_iov = &io;
-		//	msg.msg_iovlen = 1;
-		//	ssize_t len;
-		//	for(;;){
-		//		memset(buf,0,sizeof(buf));
-		//		len = recvmsg(udp_socket, &msg, 0);
-		//		if(msg.msg_flags & MSG_TRUNC){
-		//			printf("trunc\n");
-		//
-		//		} 
-		//		printf("%s\n", buf);
-		//	}
-
 	}
 }
